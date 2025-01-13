e336315838af76d08492403be8f5fa9b851404f7
Alex Shagov <shagov25@gmail.com>
2025-01-14 01:09:12 +0300

    try fix: move include under header location

--- exit.php
+++ exit.php
@@ -5,4 +5,3 @@ session_start();
 unset($_SESSION['username']);
 
 header('Location: index.php');
-
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 declare(strict_types=1);
 session_start();
-include('web/index.html');
 
 if(isset($_SESSION['username'])) {
     header('Location: web/index.html?username='. $_SESSION['username']);
 }
 
-
+include('web/index.html');
